c223c50b7e71912e538ad6dbca482266c27dfc98
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-14 18:52:20 +0200

    BP-67 Respondent migration

--- api/database/migrations/2022_09_14_162711_create_respondents_table.php
+++ api/database/migrations/2022_09_14_162711_create_respondents_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('respondents', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
+            $table->string('email')->unique();
+            $table->string('link')->unique();
             $table->timestamps();
         });
     }
