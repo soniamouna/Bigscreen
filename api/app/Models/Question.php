000a5946793bfcf78b77583e86f3c3a7b7b3c8e7
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-14 21:55:58 +0200

    BP-70 Model Question

--- api/app/Models/Question.php
+++ api/app/Models/Question.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'title',
+        'type',
+        'choices',
+    ];
+
+    public function responses() {
+        return $this->hasMany(Response::class);
+    }
+
+    public static function getAll()
+    {
+        $questions = Question::all();
+        return $questions;
+    }
 }
