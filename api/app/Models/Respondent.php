a9aa3b45c74c4ebd845cece6f135d3d8ff686577
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-14 22:05:50 +0200

    BP-72 Model Respondent

--- api/app/Models/Respondent.php
+++ api/app/Models/Respondent.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Respondent extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['email','link'];
+
+    public function responses() {
+        return $this->hasMany(Response::class);
+    }
+
+    public static function getAll(): object
+    {
+        $respondents = Response::all();
+        
+        return $respondents;
+    }
+
+    public static function getByLink($link) {
+        return Respondent::with(['responses'])->where('link', $link)->first();
+    }
 }
