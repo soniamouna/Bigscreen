749e4c174eb25889d3861786c6f6182edfbc9e3b
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-09-16 00:28:24 +0200

    BP-75 BP-80 Login part create with controller and route

---
+++ api/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+   
+class AuthController extends BaseController
+{  
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $userEmail=User::where('email', $request->email)->first(); // Get the user with the same email as the request
+        $userPassword=User::where('password', $request->password)->first();// Get the user with the same password as the request
+        //if the credentials match with the admin'credentials -> create token and send the response
+        if($userEmail && $userPassword){
+            $success['token'] =  $userEmail->createToken('user-token')->plainTextToken; 
+   
+            return $this->sendResponse($success, 'User login successfully.');
+        }else{
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']); //Otherwise send an error message
+        }
+        
+           
+         
+        
+    }
+}
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\QuestionController;
 use App\Http\Controllers\Api\ResponseController;
+use App\Http\Controllers\Api\AuthController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::resource('questions', QuestionController::class);
 Route::resource('responses', ResponseController::class);
 
+Route::controller(AuthController::class)->group(function(){
+    Route::post('login', 'login');
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
