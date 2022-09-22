3517f0f049cb8d0222ec987f898813d25c8efdd7
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-22 17:26:59 +0200

    Retrieving questions and displaying the question's informations

---
+++ front/src/components/public/FormQuestions.vue
@@ -0,0 +1,44 @@
+<script>
+    export default {
+        props: {
+        questions: {
+            type: Array,
+            default: () => []
+        }
+    },
+        
+    }
+    </script>
+    
+    <template>
+        <form >
+            <!-- Informations about the question -->
+            <div v-for="question,i in questions" :key="question.id">
+            <div>
+                <h1>Question {{question.id}}/{{questions.length}} </h1>
+                <p>{{question.title}}</p>
+            </div>
+            <!-- Choices -->
+            <div>
+                <!-- Type A -->
+                <div>
+                    <input type="text" name="" id="">
+                </div>
+                <!-- Type B -->
+                <div>
+                    <textarea type="text" name="" id=""></textarea>
+                </div>
+                <!-- Type C -->
+                <div>
+                    <input type="text" name="" id="">
+                </div>
+            </div></div>
+            <button>Finaliser</button>
+        </form>
+    </template>
+    
+    
+    
+    <style>
+    
+    </style>
--- front/src/views/public/Poll.vue
+++ front/src/views/public/Poll.vue
@@ -1,10 +1,25 @@
 <script>
+    import axios from 'axios';
+import FormQuestions from '../../components/public/FormQuestions.vue';
     export default {
-    
-    }
+    components: { FormQuestions },
+    data() {
+            return {
+                questions: [],
+            };
+        },
+        mounted() {
+            axios.get("http://127.0.0.1:8000/api/questions")
+                .then((response) => {
+                this.questions = response.data;
+            })
+                .catch(err => console.log(err));
+        },
+}
     </script>
     
     <template>
+        <FormQuestions :questions="this.questions"/>
     </template>
     
     
