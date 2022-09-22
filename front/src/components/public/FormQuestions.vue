66b280453e364e9e431413773acfe78f7de78962
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-22 19:09:26 +0200

    Retrieving  the user's responses

--- front/src/components/public/FormQuestions.vue
+++ front/src/components/public/FormQuestions.vue
@@ -1,44 +1,68 @@
 <script>
-    export default {
-        props: {
-        questions: {
-            type: Array,
-            default: () => []
-        }
+export default {
+  props: {
+    questions: {
+      type: Array,
+      default: () => [],
     },
-        
-    }
-    </script>
-    
-    <template>
-        <form >
-            <!-- Informations about the question -->
-            <div v-for="question,i in questions" :key="question.id">
-            <div>
-                <h1>Question {{question.id}}/{{questions.length}} </h1>
-                <p>{{question.title}}</p>
-            </div>
-            <!-- Choices -->
-            <div>
-                <!-- Type A -->
-                <div>
-                    <input type="text" name="" id="">
-                </div>
-                <!-- Type B -->
-                <div>
-                    <textarea type="text" name="" id=""></textarea>
-                </div>
-                <!-- Type C -->
-                <div>
-                    <input type="text" name="" id="">
-                </div>
-            </div></div>
-            <button>Finaliser</button>
-        </form>
-    </template>
-    
-    
-    
-    <style>
-    
-    </style>
+  },
+  data() {
+    return {
+      responses: [],
+    };
+  },
+  methods: {
+    addResponses() {
+      console.log(this.responses);
+    },
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="addResponses">
+    <!-- Informations about the question -->
+    <div v-for="(question, i) in questions" :key="question.id">
+      <div>
+        <h1>Question {{ question.id }}/{{ questions.length }}</h1>
+        <p>{{ question.title }}</p>
+      </div>
+      <!-- Choices -->
+      <div>
+        <!-- Type A -->
+        <div v-if="question.type === 'A'">
+          <div v-for="(choice, index) in question.choices" :key="index">
+            <input
+              type="radio"
+              :value="choice"
+              :name="question.id"
+              v-model="responses[i]"
+              required
+            />
+            <label> {{ choice }} </label>
+          </div>
+        </div>
+        <!-- Type B -->
+
+        <div v-else-if="question.type === 'B'">
+          <input
+            type="email"
+            maxlength="255"
+            v-if="question.id === 1"
+            v-model="responses[i]"
+          />
+          <textarea maxlength="255" v-else v-model="responses[i]" required></textarea>
+        </div>
+        <!-- Type C -->
+
+        <div v-else-if="question.type === 'C'" v-for="index in 5" :key="index">
+          <input type="radio" :value="index" :name="question.id" v-model="responses[i]" required />
+          <label> {{ index }} </label>
+        </div>
+      </div>
+    </div>
+    <button>Finaliser</button>
+  </form>
+</template>
+
+<style></style>
