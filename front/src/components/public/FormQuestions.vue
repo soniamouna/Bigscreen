5d12ecef3ef612a6c074e06b40dca8aaada789d7
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-23 02:02:41 +0200

    Retrieving responses in database and displaying the link in the modal BP-47

--- front/src/components/public/FormQuestions.vue
+++ front/src/components/public/FormQuestions.vue
@@ -1,68 +1,80 @@
 <script>
+import axios from 'axios';
+import ModalLinkResponses from './ModalLinkResponses.vue';
 export default {
-  props: {
-    questions: {
-      type: Array,
-      default: () => [],
+    props: {
+        questions: {
+            type: Array,
+            default: () => [],
+        },
     },
-  },
-  data() {
-    return {
-      responses: [],
-    };
-  },
-  methods: {
-    addResponses() {
-      console.log(this.responses);
+    data() {
+        return {
+            responses: [],
+            show: false,
+            link: ""
+        };
     },
-  },
+    methods: {
+        async addResponses() {
+            console.log(this.responses)
+            await axios.post("http://127.0.0.1:8000/api/responses", {email:this.responses[0],responses:this.responses})
+                .then(response => {
+                    console.log(response)
+                    this.link=response.data.link
+                })
+                .catch(error => console.log(error)); 
+        },
+    },
+   
+    components: { ModalLinkResponses }
 };
 </script>
 
 <template>
-  <form @submit.prevent="addResponses">
-    <!-- Informations about the question -->
-    <div v-for="(question, i) in questions" :key="question.id">
-      <div>
-        <h1>Question {{ question.id }}/{{ questions.length }}</h1>
-        <p>{{ question.title }}</p>
-      </div>
-      <!-- Choices -->
-      <div>
-        <!-- Type A -->
-        <div v-if="question.type === 'A'">
-          <div v-for="(choice, index) in question.choices" :key="index">
-            <input
-              type="radio"
-              :value="choice"
-              :name="question.id"
-              v-model="responses[i]"
-              required
-            />
-            <label> {{ choice }} </label>
-          </div>
-        </div>
-        <!-- Type B -->
+    <div>
+            <!-- Informations about the question -->
+            <form>
+                <!-- Informations about the question -->
+                <div v-for="(question, i) in questions" :key="question.id">
+                    <div>
+                        <h1>Question {{ question.id }}/{{ questions.length }}</h1>
+                        <p>{{ question.title }}</p>
+                    </div>
+                    <!--Choices -->
+                    <div>
+                        <!-- Type A -->
+                        <div v-if="question.type === 'A'">
+                            <div v-for="(choice, index) in question.choices" :key="index">
+                                <input type="radio" :value="choice" :name="question.id" v-model="responses[i]"
+                                    required />
+                                <label> {{ choice }} </label>
+                            </div>
+                        </div>
+                        <!-- Type B -->
 
-        <div v-else-if="question.type === 'B'">
-          <input
-            type="email"
-            maxlength="255"
-            v-if="question.id === 1"
-            v-model="responses[i]"
-          />
-          <textarea maxlength="255" v-else v-model="responses[i]" required></textarea>
-        </div>
-        <!-- Type C -->
+                        <div v-else-if="question.type === 'B'">
+                            <input type="email" v-if="question.id === 1" maxlength="255" v-model="responses[i]" />
+                            <textarea maxlength="255" v-else v-model="responses[i]" required></textarea>
+                        </div>
+                        <!-- Type C -->
 
-        <div v-else-if="question.type === 'C'" v-for="index in 5" :key="index">
-          <input type="radio" :value="index" :name="question.id" v-model="responses[i]" required />
-          <label> {{ index }} </label>
-        </div>
-      </div>
+                        <div v-else-if="question.type === 'C'" v-for="index in 5" :key="index">
+                            <input type="radio" :value="index" :name="question.id" v-model="responses[i]" required />
+                            <label> {{ index }} </label>
+                        </div>
+                    </div>
+                </div>
+
+            
+            <!-- Display modal when we  -->
+            <div>
+                <ModalLinkResponses :link="this.link"  :addResponses="this.addResponses"/>
+            </div>
+        </form>
     </div>
-    <button>Finaliser</button>
-  </form>
 </template>
 
-<style></style>
+<style>
+
+</style>
