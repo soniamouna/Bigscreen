01efc43292f36826883125a80821d327f2674dca
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-09-24 23:59:38 +0200

    Display when the poll was created

--- front/src/views/public/Responses.vue
+++ front/src/views/public/Responses.vue
@@ -1,54 +1,74 @@
 <script >
-    import axios from 'axios';
-    import { useRoute } from 'vue-router';
+import axios from 'axios';
+import { useRoute } from 'vue-router';
 import ResponsesQuestions from '../../components/public/ResponsesQuestions.vue';
-    export default {
-        components: {ResponsesQuestions },
-        data() {
-            return {
-                responses: [],
-                questions:[],
-            };
-        },
-        mounted() {
-            const route = useRoute();
-            const link = route.params.link;
-    
-         
-                axios.get("http://127.0.0.1:8000/api/questions")
-                    .then((response) => {
-                    this.questions = response.data;
-                    console.log(this.questions)
-                })
-                    .catch(err => console.log(err));
+export default {
+    components: { ResponsesQuestions },
+    data() {
+        return {
+            responses: [],
+            questions: [],
+            createdAt:""
             
-                axios.get("http://127.0.0.1:8000/api/respondents/" + link)
-                    .then((response) => {
-                        console.log(response)
-                        const idRespondent = response.data.respondent.id
-                        console.log(idRespondent)
-                        axios.get("http://127.0.0.1:8000/api/responses/" + idRespondent)
-                            .then((res) => {
-                                this.responses = res.data
-                                console.log(this.responses)
-                            })
-                            .catch(err => console.log(err));
-    
-    
+        };
+    },
+
+    methods: {
+        getDateSurvey(date){
+            const dateToString = new Date(date);
+            const day = dateToString.getDate();
+            const month = dateToString.getMonth();
+            const year = dateToString.getFullYear();
+            const hours = dateToString.getHours();
+            const minutes = dateToString.getMinutes();
+            return day+"/"+month+"/"+year+" à "+ hours+"h"+minutes;
+             
+
+
+        }
+    },
+    mounted() {
+        const route = useRoute();
+        const link = route.params.link;
+
+
+        axios.get("http://127.0.0.1:8000/api/questions")
+            .then((response) => {
+                this.questions = response.data;
+                console.log(this.questions)
+            })
+            .catch(err => console.log(err));
+
+        axios.get("http://127.0.0.1:8000/api/respondents/" + link)
+            .then((response) => {
+                this.createdAt=this.getDateSurvey(response.data.respondent.created_at)
+                const idRespondent = response.data.respondent.id
+                console.log(idRespondent)
+                axios.get("http://127.0.0.1:8000/api/responses/" + idRespondent)
+                    .then((res) => {
+                        this.responses = res.data
                     })
                     .catch(err => console.log(err));
-    
-        },
-    
-    }
-    </script>
+
+
+            })
+            .catch(err => console.log(err));
+
+    },
+
+}
+</script>
         
-    <template>
-        <ResponsesQuestions :questions="this.questions" :responses="this.responses"/>
-    </template>
+<template>
+    <div>
+        <h1>BIGSCREEN</h1>
+        <p>Vous trouverez ci-dessous les réponses que vous avez apportées à notre sondage le {{this.createdAt}}</p>
+        <ResponsesQuestions :questions="this.questions" :responses="this.responses" />
+    </div>
+</template>
         
         
         
-    <style>
-    
-    </style>
+<style>
+
+</style>
