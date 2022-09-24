3fcccee6227e17449943cd881aa23a519fc5380b
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-09-24 23:23:23 +0200

    Responses User page BP-45

---
+++ front/src/components/public/ResponsesQuestions.vue
@@ -0,0 +1,40 @@
+<script>
+    import axios from 'axios';
+    export default {
+    
+        props: {
+            questions: {
+                type: Array,
+                default: () => []
+            },
+            responses: {
+                type: Array,
+                default: () => []
+            }
+        },
+    
+    
+    }
+    </script>
+    
+    <template>
+        <div >
+            <div v-for="question,i in this.questions" :key="question.id">
+                <h1>Question {{question.id}} / {{questions.length}}</h1>
+                <p>{{question.title}}</p> 
+                <div>
+                    <p>{{responses[i].value}}</p>
+                </div>
+            </div>
+           
+
+                
+                
+    </div>
+    </template>
+    
+    
+    
+    <style>
+    
+    </style>
--- front/src/views/public/Responses.vue
+++ front/src/views/public/Responses.vue
@@ -1,15 +1,54 @@
-<script>
+<script >
+    import axios from 'axios';
+    import { useRoute } from 'vue-router';
+import ResponsesQuestions from '../../components/public/ResponsesQuestions.vue';
     export default {
+        components: {ResponsesQuestions },
+        data() {
+            return {
+                responses: [],
+                questions:[],
+            };
+        },
+        mounted() {
+            const route = useRoute();
+            const link = route.params.link;
+    
+         
+                axios.get("http://127.0.0.1:8000/api/questions")
+                    .then((response) => {
+                    this.questions = response.data;
+                    console.log(this.questions)
+                })
+                    .catch(err => console.log(err));
+            
+                axios.get("http://127.0.0.1:8000/api/respondents/" + link)
+                    .then((response) => {
+                        console.log(response)
+                        const idRespondent = response.data.respondent.id
+                        console.log(idRespondent)
+                        axios.get("http://127.0.0.1:8000/api/responses/" + idRespondent)
+                            .then((res) => {
+                                this.responses = res.data
+                                console.log(this.responses)
+                            })
+                            .catch(err => console.log(err));
+    
+    
+                    })
+                    .catch(err => console.log(err));
+    
+        },
     
     }
     </script>
-    
+        
     <template>
-    
+        <ResponsesQuestions :questions="this.questions" :responses="this.responses"/>
     </template>
-    
-    
-    
+        
+        
+        
     <style>
     
     </style>
