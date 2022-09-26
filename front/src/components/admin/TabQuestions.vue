2a58bba0cced366567616c60f75dcb1bb9feb17a
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-09-27 01:01:15 +0200

    Display Table with all questions from database BP-55

---
+++ front/src/components/admin/TabQuestions.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        props: {
+            questions: {
+                type: Array,
+                default: () => []
+            }
+        },
+    }
+    </script>
+    
+    <template>
+        <div>
+       
+            <table class="table table-bordered">
+                <tr>
+                    <th>N°</th>
+                    <th>Questions</th>
+                    <th>Types</th>
+                </tr>
+                <tr v-for="question in questions" :key="question.id">
+                    <td>{{question.id}}</td>
+                    <td>{{question.title}}</td>
+                    <td>{{question.type}}</td>
+                </tr>
+                
+    
+            </table>
+        </div>
+    </template>
+    
+    
+    
+    <style>
+    
+    </style>
--- front/src/views/admin/AdminQuestions.vue
+++ front/src/views/admin/AdminQuestions.vue
@@ -1,11 +1,26 @@
 <script>
-    export default {
-    
+    import axios from 'axios';
+    import TabQuestions from '../../components/admin/TabQuestions.vue';
+        export default {
+        components: { TabQuestions },
+        data() {
+                return {
+                    questions: [],
+                };
+            },
+            mounted() {
+                axios.get("http://127.0.0.1:8000/api/questions")
+                    .then((response) => {
+                        console.log(response)
+                    this.questions = response.data;
+                })
+                    .catch(err => console.log(err));
+            },
     }
-    </script>
+        </script>
     
     <template>
-    <div>questions</div>
+      <TabQuestions :questions="this.questions"/>
     </template>
     
     
