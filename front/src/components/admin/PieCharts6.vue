3163867c5ab2ae62f8be70b54e448258fb012f1d
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-29 01:56:45 +0200

    PieCharts with controller for piecharts responses

---
+++ front/src/components/admin/PieCharts6.vue
@@ -0,0 +1,73 @@
+
+<script>
+import Chart from 'chart.js/auto';
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            labels: [],
+            chartDatas: []
+        }
+    },
+    async mounted() {
+        const token = localStorage.getItem("token")
+
+        await axios.get("http://127.0.0.1:8000/api/infosVR/6", {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+        })
+            .then((response) => {
+                this.labels = response.data.labels
+                this.chartDatas = response.data.chartDatas
+
+            })
+            .catch(err => console.log(err));
+
+        const ctx = document.getElementById('myChart6');
+
+        const data = {
+            labels: this.labels,
+            datasets: [{
+                label: 'My First Dataset',
+                data: this.chartDatas,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(154, 162, 235)',
+                    'rgb(255, 335, 86)'
+                ],
+                hoverOffset: 4
+            }]
+        };
+
+        const myChart6 = new Chart(ctx, {
+            type: 'doughnut',
+            data: data,
+        });
+        
+
+        return myChart6;
+
+    }
+}
+
+
+</script>
+    
+    
+<template>
+
+    <div>
+        <canvas id="myChart6" width="400" height="400"></canvas>
+
+    </div>
+
+</template>
+    
+    
+<style>
+
+</style>
--- front/src/components/admin/TabResponses.vue
+++ front/src/components/admin/TabResponses.vue
@@ -18,17 +18,26 @@ export default {
 </script>
     
 <template>
-<div>
-    <table class="col-lg-12 m-auto mt-5 " v-for="response in responses" :key="response.id">        <tr>
+<div class="responsesBackground ">
+    <div class="col-lg-12 mt-4 text-center">
+        <div class=" col-lg-3 m-auto">
+            <h1 class="responsesTitlePage mb-5 font-orbitron">
+                RÉPONSES
+            </h1> 
+        </div>
+        
+    </div>
+    <table class="tabResponsesAppearance container mb-4 col-lg-12 m-auto mt-4 " v-for="response in responses" :key="response.id">
+        <tr class="text-white text-center">
             <th>N°</th>
             <th>Questions</th>
             <th>Réponses</th>
         </tr>
 
         <tr v-for="question,i in questions" :key="question.id" class="col-lg-12">
-            <td class="col-lg-2">{{question.id}}</td>
+            <td class="text-center col-lg-2">{{question.id}}</td>
             <td class="col-lg-8">{{question.title}}</td>
-            <td class="col-lg-4">{{response[i].value}}</td>
+            <td class="text-center col-lg-4">{{response[i].value}}</td>
         </tr>
 
 
@@ -43,5 +52,23 @@ tr,
 td,
 th {
     border: 1px solid grey;
+    font-family: monospace;
+    
 }
+
+tr>th{
+    background-color: rgb(117, 129, 244);
+}
+
+.tabResponsesAppearance{
+    background-color: rgba(255, 255, 255, 0.694);
+    backdrop-filter: blur(22px);
+}
+
+
+.responsesTitlePage{
+    border-bottom: 1px solid white;
+    font-weight: bolder;
+}
+
 </style>
