3a3fb5ef219687646453d9b6237041566a433161
Jonathan charlet <jonathan.charlet.pro@gmail.com>
2022-09-29 22:53:47 +0200

    CSS charts page

--- front/src/views/admin/AdminCharts.vue
+++ front/src/views/admin/AdminCharts.vue
@@ -9,16 +9,48 @@ export default {
 </script>
 
 <template>
-    <div>
-        <PieCharts6/>
-        <PieCharts7/>
-        <PieCharts10/>
-        <RadarCharts />
+    <div class="col-lg-12">
+        <div class="col-lg-12 m-auto">
+            <div class="text-center mb-4">
+                <h1 class="m-auto col-lg-2 titleWelcome font-monospace">
+                    ACCUEIL
+                </h1>
+            </div>
+            <div class="m-auto pb-4 col-lg-9 row">
+                <div class="m-auto chartsContainer p-4 col-lg-5">
+                    <PieCharts6/>
+                </div>
+                <div class="m-auto chartsContainer p-4 col-lg-5">
+                    <PieCharts7/>
+                </div>
+    
+            </div>
+            <div class="m-auto col-lg-9 row"> 
+                <div class="m-auto chartsContainer p-4 col-lg-5">   
+                    <PieCharts10/>
+                </div>
+                <div class="m-auto chartsContainer p-4 col-lg-5">
+                    <RadarCharts />
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
 
 
 <style>
+
+    .chartsContainer{
+        border: 1px solid black;
+        border-radius: 22px;
+    }
+
+    .titleWelcome{
+        font-weight: bolder;
+        border-bottom: 1px solid black;
+    }
+    
 
 </style>
