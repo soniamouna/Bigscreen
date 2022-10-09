652ebcf643f84ac52f4d7f4d7337c45b50ff8ba3
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-10-09 18:13:46 +0200

    supp logout function in admin

--- front/src/views/admin/Admin.vue
+++ front/src/views/admin/Admin.vue
@@ -13,31 +13,7 @@ export default {
             apiURL: import.meta.env.VITE_BASE_API,
         };
     },
-    methods: {
-        // methods to loggout from the admin
-        async logout() {
-            const token = localStorage.getItem("token"); //get the token in the localStorage
-            await axios.post(this.apiURL + "logout", null, {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + token,
-                },
-            })
-                .then((response) => {
-                    localStorage.removeItem("token"); //remove the token into the localStorage
-                    window.location.href = "/login"; // redirect to Login page
-                })
-                .catch(err => {
-                    console.log(err);
-                    if (err.response.status == 401) {
-                        alert("Vous êtes déjà déconnecté(e)");
-                        window.location.href = "/login";
-                    }
-                    alert("Une erreur est survenue. Veuillez recharger la page ultérieurement.");
-                    window.location.href = "/login";
-                });
-        }
-    },
+   
     mounted() {
         // chek if token exist or not 
         if (!localStorage.getItem("token")) {
